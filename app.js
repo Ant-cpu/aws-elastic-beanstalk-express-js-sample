b14e4b60291e6c23f7a532e137755ce4010f3042
Anton Honchar <anton.h@Maxitechs.local>
2020-11-24 16:21:33 +0200

    second change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('https://aws.amazon.com/getting-started/hands-on/create-continuous-delivery-pipeline/module-one/?e=gs2020&p=cicd-intro'));
+app.get('/', (req, res) => res.send(':('));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
